fa7865b493e32bdbdf753528b9d4f268d67b34b0
artema1981 <artema1981@gmail.com>
2022-12-25 09:33:34 +0200

    add gallery

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import DishCategory, Dish
+from .models import DishCategory, Dish, Events, Gallery, Whuus
 # Register your models here.
 admin.site.register(DishCategory)
 admin.site.register(Dish)
+admin.site.register(Events)
+admin.site.register(Whuus)
+admin.site.register(Gallery)
--- main_page/views.py
+++ main_page/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from .models import DishCategory, Dish
+from .models import DishCategory, Dish, Events, Gallery, Whuus
 from django.http import HttpResponse
+import random
 # Create your views here.
 def main_page(request):
     categories = DishCategory.objects.filter(is_visible=True)
     dishes = Dish.objects.filter(is_visible=True, is_special=False)
     specials_dishes = Dish.objects.filter(is_visible=True, is_special=True)
+    events = Events.objects.filter(is_visible=True)
+    gallery_photos = list(Gallery.objects.filter(is_visible=True))
+    gallery_photos = random.sample(gallery_photos, 8)
 
     return render(request, 'main_page.html', context={
         'categories': categories,
         'dishes': dishes,
-        'special': specials_dishes
+        'special': specials_dishes,
+        'events': events,
+        'gallery_photos': gallery_photos,
     })
     # return HttpResponse('\n'.join(map(str, categories)) + '\n'.join(map(str, dishes)))
